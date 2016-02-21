a1fbae0b23b13178cb5e41cf1d98d54311507c01
Evan Turner <evturn@gmail.com>
2016-02-21 14:52:51 -0500

    Create CurrentWeather struct

---
+++ tormenta/CurrentWeather.swift
@@ -0,0 +1,24 @@
+//
+//  CurrentWeather.swift
+//  tormenta
+//
+//  Created by Evan Turner on 2/21/16.
+//  Copyright © 2016 Evan Turner. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+  
+  let temperature: Int
+  let humidity: Int
+  let precipProbability: Int
+  let summary: String
+  
+  init(weatherDictionary: [String: AnyObject]) {
+    temperature = weatherDictionary["temperature"] as! Int
+    humidity = weatherDictionary["humidity"] as! Int
+    precipProbability = weatherDictionary["precipChance"] as! Int
+    summary = weatherDictionary["summary"] as! String
+  }
+}
