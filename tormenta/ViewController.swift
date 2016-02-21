126edcf89f8ef6422be4ce8f60a3e0819dd429d7
Evan Turner <evturn@gmail.com>
2016-02-21 17:48:31 -0500

    Add constraints to labels

--- tormenta/ViewController.swift
+++ tormenta/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
   
   @IBOutlet weak var currentTemperatureLabel: UILabel?
+  @IBOutlet weak var currentHumidityLabel: UILabel?
+  @IBOutlet weak var currentPrecipitationLabel: UILabel?
   
 
   override func viewDidLoad() {
@@ -20,9 +22,12 @@ class ViewController: UIViewController {
     if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
     let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
     let currentWeatherDictionary = weatherDictionary["currently"] as? [String: AnyObject] {
-      let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)]
+      let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
       
       currentTemperatureLabel?.text = "\(currentWeather.temperature)º"
+      currentHumidityLabel?.text = "\(currentWeather.humidity)%"
+      currentPrecipitationLabel?.text = "\(currentWeather.precipProbability)%"
+
     }
     
   }
