a99e6bf6cf450fe7e0aa994add5043f370bdbaff
Evan Turner <evturn@gmail.com>
2016-02-22 13:56:40 -0500

    Create downloadJSONFromURL function in NetworkOperation class

--- tormenta/NetworkOperation.swift
+++ tormenta/NetworkOperation.swift
@@ -14,8 +14,32 @@ class NetworkOperation {
   lazy var session: NSURLSession = NSURLSession(configuration: self.config)
   let queryURL: NSURL
   
+  typealias JSONDictionaryCompletion = ([String: AnyObject]?) -> ()
+  
   init(url: NSURL) {
     self.queryURL = url
+  }
+  
+  func downloadJSONFromURL(completion: JSONDictionaryCompletion) {
+    let request: NSURLRequest = NSURLRequest(URL: queryURL)
+    let dataTask = session.dataTaskWithRequest(request) {
+      (let data, let response, let error) in
+      
+      if let httpResponse = response as? NSHTTPURLResponse {
+        
+        switch(httpResponse.statusCode) {
+          case 200:
+            let jsonDictionary = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as? [String: AnyObject]
+            completion(jsonDictionary)
+          default:
+            print("GET request not successful. HTTP status code \(httpResponse.statusCode)")
+        }
+      } else {
+        print("Error: Not a valid HTTP Repsonse")
+      }
+    }
+    
+    dataTask.resume()
   }
   
 }
