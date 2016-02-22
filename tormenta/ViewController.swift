b73fd3fd6130466528f05623977b19543c6a7a95
Evan Turner <evturn@gmail.com>
2016-02-22 16:30:22 -0500

    Remove old viewDidLoad logic referencing pList

--- tormenta/ViewController.swift
+++ tormenta/ViewController.swift
@@ -16,25 +16,19 @@ class ViewController: UIViewController {
   
   private let forecastAPIKey = "1f46e094ffe74026e1406789fbe04788"
   
+  let coordinate: (lat: Double, long: Double) = (37.8267,-122.423)
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(forecastAPIKey)/")
-    let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+    let forecastService = ForecastService(APIKey: forecastAPIKey)
     
-    let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
-    let session = NSURLSession(configuration: configuration)
-    
-    let request = NSURLRequest(URL: forecastURL!)
-    
-    let dataTask = session.dataTaskWithRequest(request, completionHandler: {
-      (data, response, error) -> Void in
-      print(data)
-      print("BACKGROUND THREAD SON!")
-    })
-    
-    print("MAIN THREAD SON!")
-    dataTask.resume()
+    forecastService.getForecast(coordinate.lat, coordinate.long) {
+      (let currently) in
+      if let currentWeather = currently {
+        
+      }
+    }
   }
 
   override func didReceiveMemoryWarning() {
