f31654a2371ae4bf3540703955b858fa0a0e3187
Evan Turner <evturn@gmail.com>
2016-02-22 11:36:31 -0500

    Add API and NSURLs

--- tormenta/ViewController.swift
+++ tormenta/ViewController.swift
@@ -14,22 +14,16 @@ class ViewController: UIViewController {
   @IBOutlet weak var currentHumidityLabel: UILabel?
   @IBOutlet weak var currentPrecipitationLabel: UILabel?
   
-
+  private let forecastAPIKey = "1f46e094ffe74026e1406789fbe04788"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(forecastAPIKey)/")
+    let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
     
-    if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
-    let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
-    let currentWeatherDictionary = weatherDictionary["currently"] as? [String: AnyObject] {
-      let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
-      
-      currentTemperatureLabel?.text = "\(currentWeather.temperature)º"
-      currentHumidityLabel?.text = "\(currentWeather.humidity)%"
-      currentPrecipitationLabel?.text = "\(currentWeather.precipProbability)%"
-
-    }
-    
+    let weatherData = NSData(contentsOfURL: forecastURL!)
+    print(weatherData)
   }
 
   override func didReceiveMemoryWarning() {
