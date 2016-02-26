52eb022879a5b57db7f033879b15664bfa0dd640
Evan Turner <evturn@gmail.com>
2016-02-26 12:26:28 -0500

    Create Icon enum file

---
+++ tormenta/DailyWeather.swift
@@ -0,0 +1,41 @@
+//
+//  DailyWeather.swift
+//  tormenta
+//
+//  Created by Evan Turner on 2/26/16.
+//  Copyright © 2016 Evan Turner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct DailyWeather {
+  
+  let maxTemperature: Int?
+  let minTemperature: Int?
+  let humidity: Int?
+  let precipChance: Int?
+  let summary: String?
+  var icon: UIImage? = UIImage(named: "default.png")
+  var largeIcon: UIImage? = UIImage(named: "default_large.png")
+  let sunriseTime: String?
+  let sunsetTime: String?
+  let day: String?
+  
+  init(dailyWeatherDict: [String: AnyObject]) {
+    maxTemperature = dailyWeatherDict["temperatureMax"] as? Int
+    if let humidityFloat = dailyWeatherDict["humidity"] as? Double {
+      humidity = Int(humidityFloat * 100)
+    } else {
+      humidity = nil
+    }
+    if let precipChanceFloat = dailyWeatherDict["precipProbability"] as? Double {
+      precipChance = Int(precipChanceFloat * 100)
+    } else {
+      precipChance = nil
+    }
+    summary = dailyWeatherDict["summary"] as? String
+    
+  }
+  
+}
