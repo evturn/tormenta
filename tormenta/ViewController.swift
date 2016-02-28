8942d580e2d912a9fd6b8d4e94d4cd01e156b769
Evan Turner <evturn@gmail.com>
2016-02-28 06:11:56 -0500

    Update detail view with current weather

--- tormenta/ViewController.swift
+++ tormenta/ViewController.swift
@@ -21,10 +21,14 @@ class ViewController: UIViewController {
   @IBOutlet weak var summaryLabel: UILabel?
   @IBOutlet weak var sunriseTimeLabel: UILabel?
   @IBOutlet weak var sunsetTimeLabel: UILabel?
+  @IBOutlet weak var lowTemperatureLabel: UILabel?
+  @IBOutlet weak var highTemperatureLabel: UILabel?
+  @IBOutlet weak var precipitationLabel: UILabel?
+  @IBOutlet weak var humidityLabel: UILabel?
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    configureView()
   }
   
   func configureView() {
@@ -33,6 +37,18 @@ class ViewController: UIViewController {
       summaryLabel?.text = weather.summary
       sunriseTimeLabel?.text = weather.sunriseTime
       sunsetTimeLabel?.text = weather.sunsetTime
+      
+      if let lowTemp = weather.minTemperature,
+        let highTemp = weather.maxTemperature,
+        let rain = weather.precipChance,
+        let humidity = weather.humidity {
+          self.lowTemperatureLabel?.text = "\(lowTemp)º"
+          self.highTemperatureLabel?.text = "\(highTemp)º"
+          self.precipitationLabel?.text = "\(rain)%"
+          self.humidityLabel?.text = "\(humidity)%"
+      }
+      
+      
       self.title = weather.day
     }
     
